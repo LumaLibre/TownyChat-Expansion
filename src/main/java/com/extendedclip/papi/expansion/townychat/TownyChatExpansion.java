8b814b2d790dbdb06621a286c5be363b07a691d4
Jsinco <jonah@jsinco.dev>
2026-02-07 13:06:46 -0500

    Add external chat channel support

--- src/main/java/com/extendedclip/papi/expansion/townychat/TownyChatExpansion.java
+++ src/main/java/com/extendedclip/papi/expansion/townychat/TownyChatExpansion.java
@@ -1,127 +1,140 @@
 package com.extendedclip.papi.expansion.townychat;
 
+import com.gmail.nossr50.api.ChatAPI;
+import com.palmergames.bukkit.TownyChat.Chat;
 import com.palmergames.bukkit.TownyChat.TownyChatFormatter;
+import com.palmergames.bukkit.TownyChat.channels.ChannelsHolder;
 import com.palmergames.bukkit.TownyChat.config.ChatSettings;
-import com.palmergames.bukkit.TownyChat.events.AsyncChatHookEvent;
+import com.palmergames.bukkit.TownyChat.events.PlayerJoinChatChannelEvent;
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.TownyUniverse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.rezzedup.discordsrv.staffchat.StaffChatAPI;
 import me.clip.placeholderapi.expansion.Cleanable;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class TownyChatExpansion extends PlaceholderExpansion implements Listener, Cleanable {
+  private final String VERSION = this.getClass().getPackage().getImplementationVersion();
+  private Map<String, TownyChatExpansion.ChatPlayer> players;
 
-  private final String VERSION = getClass().getPackage().getImplementationVersion();
-
-  private Map<String, ChatPlayer> players;
-
-  @Override
-  public String getIdentifier() {
+  public @NotNull String getIdentifier() {
     return "townychat";
   }
 
-  @Override
   public String getRequiredPlugin() {
     return "TownyChat";
   }
 
-  @Override
-  public String getAuthor() {
+  public @NotNull String getAuthor() {
     return "clip";
   }
 
-  @Override
-  public String getVersion() {
-    return VERSION;
+  public @NotNull String getVersion() {
+    return this.VERSION;
   }
 
-  @Override
+  public TownyChatExpansion() {
+    Bukkit.getPluginManager().registerEvents(this, Towny.getPlugin());
+  }
+
   public String onPlaceholderRequest(Player p, String identifier) {
-
     switch (identifier) {
-      case "channel_tag":
-        return getChatPlayer(p).getTag();
-      case "channel_name":
-        return getChatPlayer(p).getChannel();
-      case "message_color":
-        return getChatPlayer(p).getColor();
-    }
-
-    try {
-
-      Resident r = TownyUniverse.getInstance().getResident(p.getName());
-
-      switch (identifier) {
-        case "world":
-          return String.format(ChatSettings.getWorldTag(), new Object[]{p.getWorld().getName()});
-        case "town":
-          return r.hasTown() ? r.getTown().getName() : "";
-        case "townformatted":
-          return TownyChatFormatter.formatTownTag(r, false, true);
-        case "towntag":
-          return TownyChatFormatter.formatTownTag(r, false, false);
-        case "towntagoverride":
-          return TownyChatFormatter.formatTownTag(r, true, false);
-        case "nation":
-          return r.hasNation() ? r.getTown().getNation().getName() : "";
-        case "nationformatted":
-          return TownyChatFormatter.formatNationTag(r, false, true);
-        case "nationtag":
-          return TownyChatFormatter.formatNationTag(r, false, false);
-        case "nationtagoverride":
-          return TownyChatFormatter.formatNationTag(r, true, false);
-        case "townytag":
-          return TownyChatFormatter.formatTownyTag(r, false, false);
-        case "townyformatted":
-          return TownyChatFormatter.formatTownyTag(r, false, true);
-        case "townytagoverride":
-          return TownyChatFormatter.formatTownyTag(r, true, false);
-        case "title":
-          return r.hasTitle() ? r.getTitle() : "";
-        case "surname":
-          return r.hasSurname() ? r.getSurname() : "";
-        case "townynameprefix":
-          return r.getNamePrefix();
-        case "townynamepostfix":
-          return r.getNamePostfix();
-        case "townyprefix":
-          return r.hasTitle() ? r.getTitle() : r.getNamePrefix();
-        case "townypostfix":
-          return r.hasSurname() ? r.getSurname() : r.getNamePostfix();
-        case "townycolor":
-          return r.isMayor() ? ChatSettings.getMayorColour() : r.isKing() ? ChatSettings.getKingColour() : ChatSettings.getResidentColour();
-        case "group":
-          return TownyUniverse.getInstance().getPermissionSource().getPlayerGroup(p);
-        case "permprefix":
-          return TownyUniverse.getInstance().getPermissionSource().getPrefixSuffix(r, "prefix");
-        case "permsuffix":
-          return TownyUniverse.getInstance().getPermissionSource().getPrefixSuffix(r, "suffix");
-        case "channeltag":
-          return TownyChatFormatter.formatTownyTag(r, Boolean.valueOf(false), Boolean.valueOf(false));
+      case "channel_tag" -> {
+        return this.getChatPlayer(p).getTag();
       }
 
-    } catch (NotRegisteredException e) {
-      return "";
+      case "channel_name" -> {
+        @Nullable String altChannel = AlternativePluginRequestHandler.INSTANCE.handle(p, identifier);
+        if (altChannel != null) {
+          return altChannel;
+        }
+
+        String s = this.getChatPlayer(p).getChannel();
+        if (!s.isEmpty()) {
+          s = s.substring(0, 1).toUpperCase() + s.substring(1);
+        } else {
+          s = "Global";
+        }
+
+        return s;
+      }
+      case "message_color" -> {
+        @Nullable String altColor = AlternativePluginRequestHandler.INSTANCE.color(p);
+        if (altColor != null) {
+          return altColor;
+        }
+
+        String channel = this.getChatPlayer(p).getChannel();
+        if (!channel.isEmpty() && !channel.equalsIgnoreCase("global")) {
+          return this.getChatPlayer(p).getColor();
+        }
+        return "";
+      }
+
+      default -> {
+        return TownyUniverseRequestHandler.INSTANCE.handle(p, identifier);
+      }
     }
-    return null;
   }
 
-  @Override
   public void cleanup(Player player) {
-    if (players != null && players.containsKey(player.getName())) {
-      players.remove(player.getName());
+    if (this.players != null && this.players.containsKey(player.getName())) {
+      this.players.remove(player.getName());
     }
   }
 
-  public class ChatPlayer {
+  private void updatePlayer(String player, String ch, String tag, String cc) {
+    if (this.players == null) {
+      this.players = new HashMap<>();
+    }
 
+    if (this.players.containsKey(player) && this.players.get(player) != null) {
+      this.players.get(player).setChannel(ch);
+      this.players.get(player).setTag(tag);
+      this.players.get(player).setColor(cc);
+    } else {
+      TownyChatExpansion.ChatPlayer pl = new TownyChatExpansion.ChatPlayer(player, ch, tag, cc);
+      this.players.put(player, pl);
+    }
+  }
+
+  private TownyChatExpansion.ChatPlayer getChatPlayer(Player p) {
+    return this.players != null && this.players.containsKey(p.getName()) && this.players.get(p.getName()) != null
+            ? this.players.get(p.getName())
+            : new TownyChatExpansion.ChatPlayer(p.getName(), "", "", "");
+  }
+
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onChat(PlayerJoinChatChannelEvent e) {
+    Player p = e.getPlayer();
+    new ChannelsHolder(Chat.getPlugin(Chat.class));
+    String tag = "";
+    String channel = "";
+    String msgColor = "";
+    if (e.getChannel() != null) {
+      tag = e.getChannel().getChannelTag();
+      channel = e.getChannel().getName();
+      msgColor = e.getChannel().getMessageColour();
+    }
+
+    this.updatePlayer(p.getName(), channel, tag, msgColor);
+  }
+
+  public static class ChatPlayer {
     private String player;
     private String channel;
     private String tag;
@@ -134,11 +147,13 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
       } else {
         this.setChannel("");
       }
+
       if (tag != null) {
         this.setTag(tag);
       } else {
         this.setTag("");
       }
+
       if (color != null) {
         this.setColor(color);
       } else {
@@ -147,7 +162,7 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
     }
 
     public String getPlayer() {
-      return player;
+      return this.player;
     }
 
     public void setPlayer(String player) {
@@ -155,10 +170,7 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
     }
 
     public String getChannel() {
-      if (channel == null) {
-        return "";
-      }
-      return channel;
+      return this.channel == null ? "" : this.channel;
     }
 
     public void setChannel(String channel) {
@@ -166,10 +178,7 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
     }
 
     public String getTag() {
-      if (tag == null) {
-        return "";
-      }
-      return tag;
+      return this.tag == null ? "" : this.tag;
     }
 
     public void setTag(String tag) {
@@ -177,10 +186,7 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
     }
 
     public String getColor() {
-      if (color == null) {
-        return "";
-      }
-      return color;
+      return this.color == null ? "" : this.color;
     }
 
     public void setColor(String color) {
@@ -188,42 +194,166 @@ public class TownyChatExpansion extends PlaceholderExpansion implements Listener
     }
   }
 
-  private void updatePlayer(String player, String ch, String tag, String cc) {
-    if (players == null) {
-      players = new HashMap<String, ChatPlayer>();
-    }
-    if (players.containsKey(player) && players.get(player) != null) {
-      players.get(player).setChannel(ch);
-      players.get(player).setTag(tag);
-      players.get(player).setColor(cc);
-    } else {
-      ChatPlayer pl = new ChatPlayer(player, ch, tag, cc);
-      players.put(player, pl);
+  public interface RequestHandler {
+    @Nullable String handle(Player p, String identifier);
+  }
+  public interface ChatRequestHandler extends RequestHandler {
+    @Nullable String color(Player p);
+  }
+
+  public static class TownyUniverseRequestHandler implements RequestHandler {
+
+    public static final TownyUniverseRequestHandler INSTANCE = new TownyUniverseRequestHandler();
+
+
+    @Override
+    @NotNull
+    public String handle(Player p, String identifier) {
+      try {
+        Resident r = TownyUniverse.getInstance().getResident(p.getName());
+        if (r == null) return "";
+
+        return switch (identifier) {
+          case "world" -> String.format(ChatSettings.getWorldTag(), p.getWorld().getName());
+          case "town" -> r.hasTown() ? r.getTown().getName() : "&7None";
+          case "townformatted" -> TownyChatFormatter.formatTownTag(r, false, true);
+          case "towntag" -> TownyChatFormatter.formatTownTag(r, false, false);
+          case "towntagoverride" -> TownyChatFormatter.formatTownTag(r, true, false);
+          case "nation" -> r.hasNation() ? r.getTown().getNation().getName() : "";
+          case "nationformatted" -> TownyChatFormatter.formatNationTag(r, false, true);
+          case "nationtag" -> TownyChatFormatter.formatNationTag(r, false, false);
+          case "nationtagoverride" -> TownyChatFormatter.formatNationTag(r, true, false);
+          case "townytag" -> TownyChatFormatter.formatTownyTag(r, false, false);
+          case "townyformatted" -> TownyChatFormatter.formatTownyTag(r, false, true);
+          case "townytagoverride" -> TownyChatFormatter.formatTownyTag(r, true, false);
+          case "title" -> r.hasTitle() ? r.getTitle() : "";
+          case "surname" -> r.hasSurname() ? r.getSurname() : "";
+          case "townynameprefix" -> r.getNamePrefix();
+          case "townynamepostfix" -> r.getNamePostfix();
+          case "townyprefix" -> r.hasTitle() ? r.getTitle() : r.getNamePrefix();
+          case "townypostfix" -> r.hasSurname() ? r.getSurname() : r.getNamePostfix();
+          case "townycolor" ->
+                  r.isMayor() ? ChatSettings.getMayorColour() : (r.isKing() ? ChatSettings.getKingColour() : ChatSettings.getResidentColour());
+          case "group" -> TownyUniverse.getInstance().getPermissionSource().getPlayerGroup(p);
+          case "permprefix" -> TownyUniverse.getInstance().getPermissionSource().getPrefixSuffix(r, "prefix");
+          case "permsuffix" -> TownyUniverse.getInstance().getPermissionSource().getPrefixSuffix(r, "suffix");
+          case "channeltag" -> TownyChatFormatter.formatTownyTag(r, false, false);
+          default -> null;
+        };
+      } catch (NotRegisteredException var7) {
+        return "";
+      }
     }
   }
 
-  private ChatPlayer getChatPlayer(Player p) {
-    return players != null && players.containsKey(p.getName()) && players.get(p.getName()) != null ?
-        players.get(p.getName()) : new ChatPlayer(p.getName(), "", "", "");
+
+  public static class AlternativePluginRequestHandler implements ChatRequestHandler {
+
+    public static final AlternativePluginRequestHandler INSTANCE = new AlternativePluginRequestHandler();
+
+    // Order matters
+    private final List<ChatRequestHandler> otherHandlers = List.of(
+            new DiscordSRVStaffChatRequestHandler(),
+            new McMMORequestHandler()
+    );
+
+
+    @Override
+    public String handle(Player p, String identifier) {
+      for (RequestHandler otherHandler : otherHandlers) {
+        String result = otherHandler.handle(p, identifier);
+        if (result != null) {
+          return result;
+        }
+      }
+      return null;
+    }
+
+    @Override
+    public @Nullable String color(Player p) {
+      for (ChatRequestHandler otherHandler : otherHandlers) {
+        String result = otherHandler.color(p);
+        if (result != null) {
+          return result;
+        }
+      }
+      return null;
+    }
   }
 
-  @EventHandler(priority = EventPriority.LOWEST)
-  public void onChat(AsyncChatHookEvent e) {
 
-    if (e.isCancelled()) {
-      return;
+  public static class McMMORequestHandler implements ChatRequestHandler {
+
+    private Boolean isEnabled = null;
+
+    @Override
+    public @Nullable String handle(Player p, String identifier) {
+      if (isEnabled() && ChatAPI.isUsingPartyChat(p)) {
+        return "mcMMO Party";
+      }
+      return null;
     }
 
-    Player p = e.getPlayer();
-    String tag = "";
-    String channel = "";
-    String msgColor = "";
-    if (e.getChannel() != null) {
-      tag = e.getChannel().getChannelTag();
-      channel = e.getChannel().getName();
-      msgColor = e.getChannel().getMessageColour();
+    @Override
+    public @Nullable String color(Player p) {
+      if (isEnabled() && ChatAPI.isUsingPartyChat(p)) {
+        return "&#FCB75C";
+      }
+      return null;
     }
 
-    updatePlayer(p.getName(), channel, tag, msgColor);
+    private boolean isEnabled() {
+      if (this.isEnabled == null) {
+        this.isEnabled = Bukkit.getPluginManager().getPlugin("mcMMO") != null;
+      }
+      return this.isEnabled;
+    }
+  }
+
+
+  public static class DiscordSRVStaffChatRequestHandler implements ChatRequestHandler {
+
+    private Boolean isEnabled = null;
+    private Plugin cachedInstance = null;
+
+    @Override
+    public @Nullable String handle(Player p, String identifier) {
+      if (!isEnabled()) {
+        return null;
+      }
+
+      if (instance().data().isAutomaticStaffChatEnabled(p)) {
+        return "Staff-Chat";
+      }
+
+      return null;
+    }
+
+    @Override
+    public @Nullable String color(Player p) {
+      if (!isEnabled()) {
+        return null;
+      }
+
+      if (instance().data().isAutomaticStaffChatEnabled(p)) {
+        return "&#a1ff68";
+      }
+
+      return null;
+    }
+
+    private boolean isEnabled() {
+      if (this.isEnabled == null) {
+        this.isEnabled = Bukkit.getPluginManager().getPlugin("DiscordSRV-Staff-Chat") != null;
+      }
+      return this.isEnabled;
+    }
+
+    private StaffChatAPI instance() {
+      if (this.cachedInstance == null) {
+        this.cachedInstance = Bukkit.getPluginManager().getPlugin("DiscordSRV-Staff-Chat");
+      }
+      return (StaffChatAPI) this.cachedInstance;
+    }
   }
 }
